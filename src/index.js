f6d6abff314be8a85247fff1a3b321068d2464a0
milli0026 <milli001@gmx.de>
2024-12-01 12:26:14 +0100

    improved user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
   let apiKey = "1f653d7bc9et5d03b45aa0oc41f43bec";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about "${instructionsInput.value}"...</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
